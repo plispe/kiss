42ed6ae3d65a5b1c5facea63a1174695ee20488c
Petr Pliska <petr.pliska@post.cz>
2016-07-10 16:09:46 +0200

    Remove if constructions

--- app/functions.php
+++ app/functions.php
@@ -40,65 +40,59 @@ use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
  */
 use AdamWathan\Form\FormBuilder;
 
-if (! function_exists('App\getStatusCode')) {
-    /**
-     * @param \Exception
-     *
-     * @return Int
-     */
-    function getStatusCode(\Throwable $e): int
-    {
-        return $e instanceof HttpExceptionInterface ?
-            $e->getStatusCode() : Response::HTTP_INTERNAL_SERVER_ERROR;
-    }
+/**
+ * @param \Exception
+ *
+ * @return Int
+ */
+function getStatusCode(\Throwable $e): int
+{
+    return $e instanceof HttpExceptionInterface ?
+        $e->getStatusCode() : Response::HTTP_INTERNAL_SERVER_ERROR;
 }
 
-if (! function_exists('App\exceptionToHtmlResponse')) {
-    /**
-     * Turns Exception object into PSR-7 HtmlMessage
-     * @see http://tracy.nette.org/en/
-     *
-     * @param \Exception
-     *
-     * @return HtmlResponse
-     */
-    function exceptionToHtmlResponse(\Throwable $e): HtmlResponse
-    {
-        ob_start();
-        (new BlueScreen())->render($e);
+/**
+ * Turns Exception object into PSR-7 HtmlMessage
+ * @see http://tracy.nette.org/en/
+ *
+ * @param \Exception
+ *
+ * @return HtmlResponse
+ */
+function exceptionToHtmlResponse(\Throwable $e): HtmlResponse
+{
+    ob_start();
+    (new BlueScreen())->render($e);
 
-        return new HtmlResponse(ob_get_clean(), getStatusCode($e));
-    }
+    return new HtmlResponse(ob_get_clean(), getStatusCode($e));
 }
 
-if (! function_exists('App\renderExceptionTemplateToHtmlResponse')) {
-    /**
-     * Turns Exception object into PSR-7 HtmlMessage
-     * @see https://latte.nette.org/en/
-     *
-     * @param \Exception
-     *
-     * @return HtmlResponse
-     */
-    function renderExceptionTemplateToHtmlResponse(\Throwable $e): HtmlResponse
-    {
-        $code = getStatusCode($e);
-        $template = sprintf(__DIR__ . '/_templates/web/error/%s.latte', getStatusCode($e));
+/**
+ * Turns Exception object into PSR-7 HtmlMessage
+ * @see https://latte.nette.org/en/
+ *
+ * @param \Exception
+ *
+ * @return HtmlResponse
+ */
+function renderExceptionTemplateToHtmlResponse(\Throwable $e): HtmlResponse
+{
+    $code = getStatusCode($e);
+    $template = sprintf(__DIR__ . '/_templates/web/error/%s.latte', getStatusCode($e));
 
-        // If specific template does not exist, template for error 500 will be rendered.
-        if (!file_exists($template)) {
-            $template = __DIR__ . '/_templates/web/error/500.latte';
-            $code = Response::HTTP_INTERNAL_SERVER_ERROR;
-        }
-
-        $params = [
-            'request' => ServerRequestFactory::fromGlobals(),
-            'builder' => new FormBuilder
-        ];
-
-        return new HtmlResponse(
-            (new Engine)->renderToString($template, $params),
-            $code
-        );
+    // If specific template does not exist, template for error 500 will be rendered.
+    if (!file_exists($template)) {
+        $template = __DIR__ . '/_templates/web/error/500.latte';
+        $code = Response::HTTP_INTERNAL_SERVER_ERROR;
     }
+
+    $params = [
+        'request' => ServerRequestFactory::fromGlobals(),
+        'builder' => new FormBuilder
+    ];
+
+    return new HtmlResponse(
+        (new Engine)->renderToString($template, $params),
+        $code
+    );
 }
